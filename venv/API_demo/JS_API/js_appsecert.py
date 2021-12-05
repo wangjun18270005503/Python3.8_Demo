8a5c450e1edd139a544967a8fe5468772057d214
wangjun <1596118071@qq.com>
2021-12-05 22:50:31 +0800

    提交请求江山密钥API验证框架

--- venv/API_demo/JS_API/js_appsecert.py
+++ venv/API_demo/JS_API/js_appsecert.py
@@ -5,3 +5,21 @@
 # @Version：V 0.1
 # @File : js_appsecert.py
 # @desc : （1）获取刷新密钥接口
+
+import requests
+class  test_api():
+    def request_appsecert():
+        secert_url = "http://szgg.jiangshan.gov.cn:81/appsecert"  # 接口地址:刷新密钥
+        body = {"appSecret":"BMZFIqnp3kVp4fhHWmxL","appKey":"acimwoFCVSz7JXJeoXw7"}
+        headers={'Content-Type':'application/json'}
+        r = requests.post(secert_url, json=body, headers=headers)  # verify = False 忽略SSH 验证
+        if len(r.text) != 0 and str(r.json()['code']) == '0':
+            print("刷新密钥请求成功！！！")
+            refreshSecret = r.json()['data']['refreshSecret']
+            requestTime = r.json()['data']['requestTime']
+            print(refreshSecret+' ------------------- '+str(requestTime))
+        return {'refreshSecret':refreshSecret,'requestTime':str(requestTime)}
+if __name__ == '__main__':
+    print('开始请求：刷新密钥')
+    print(test_api.request_appsecert())
+    print('结束请求：刷新密钥')
--- venv/API_demo/JS_API/js_interfaceSecurity.py
+++ venv/API_demo/JS_API/js_interfaceSecurity.py
@@ -4,4 +4,26 @@
 # @Author : J.wang
 # @Version：V 0.1
 # @File : js_interfaceSecurity.py
-# @desc :
+# @desc : (2)刷新请求密钥接口
+
+import requests
+from js_appsecert import test_api
+def request_interfaceSecurity(appsecert):
+    interfaceSecurity_url = "http://szgg.jiangshan.gov.cn:81/interfaceSecurity"   # 接口地址:请求密钥
+    # body = {"refreshSecret": appsecert['refreshSecret'],"appKey":"acimwoFCVSz7JXJeoXw7","requestTime": appsecert['requestTime']}
+    body_2 = {"refreshSecret": "b7d78d48a7a2a0b95b56abe946bb4e48","appKey":"acimwoFCVSz7JXJeoXw7","requestTime": "1638715688647"}
+    headers ={'Content-Type':'application/json'}
+    r = requests.post(interfaceSecurity_url, json=body_2, headers=headers)  # verify = False 忽略SSH 验证
+    print(r)
+    if r == None and str(r.json()['code']) == '0':
+        print("2刷新密钥请求成功！！！")
+        requestSecret = r.json()['data']['requestSecret']
+        requestTime = r.json()['data']['requestTime']
+        print(requestSecret+' ------------------- '+str(requestTime))
+    # return {'requestSecret':requestSecret,'requestTime':requestTime}
+if __name__ == '__main__':
+    print('开始请求：请求密钥')
+    a = test_api.request_appsecert()
+    print(a['refreshSecret'])
+    print(request_interfaceSecurity(a))
+    print('结束请求：请求密钥')
