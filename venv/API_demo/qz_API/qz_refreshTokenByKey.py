27a3c2368d5aa16c4913c513c03309a24b5635c9
wangjun <1596118071@qq.com>
2021-12-05 21:29:30 +0800

    提交请求衢州密钥API验证

---
+++ venv/API_demo/qz_API/qz_refreshTokenByKey.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2021/12/5 21:28
+# @Author : J.wang
+# @Version：V 0.1
+# @File : qz_refreshTokenByKey.py
+# @desc : （1）刷新密钥
---
+++ venv/API_demo/qz_API/qz_refreshTokenBySec.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2021/12/5 21:26
+# @Author : J.wang
+# @Version：V 0.1
+# @File : qz_refreshTokenBySec.py
+# @desc : （1）请求密钥
