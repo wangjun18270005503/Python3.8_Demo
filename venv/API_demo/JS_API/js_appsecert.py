cf9673e82949005deb702976edc0c6a83730ac51
wangjun <1596118071@qq.com>
2021-12-05 21:32:06 +0800

    提交请求江山密钥API验证框架

---
+++ venv/API_demo/JS_API/js_appsecert.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2021/12/5 21:30
+# @Author : J.wang
+# @Version：V 0.1
+# @File : js_appsecert.py
+# @desc : （1）获取刷新密钥接口
---
+++ venv/API_demo/JS_API/js_interfaceSecurity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# @Time : 2021/12/5 21:31
+# @Author : J.wang
+# @Version：V 0.1
+# @File : js_interfaceSecurity.py
+# @desc :
